ac310b177a5f5e21c5672f08452244efde1c3702
Matthew James Davis <davis.matthewjames@gmail.com>
2016-07-25 15:10:51 -0400

    update contact links add subscription button

---
+++ subscription.js
@@ -0,0 +1,15 @@
+var element = document.getElementById('subscription-button');
+element.addEventListener('click', function(event) {
+	var email = window.prompt('What is your email address?');
+	if (!email) {
+		return;
+	}
+	var request = new XMLHttpRequest();
+	request.open('POST', '//davismj.us11.list-manage.com/subscribe/post');
+	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	request.setRequestHeader('Accept', 'application/json');
+	request.addEventListener('load', onload);
+	request.addEventListener('error', onload);
+	request.send('u=085d73ebee6205ba545fd7a00&id=64aca48f63&MERGE0=' + email);
+	function onload() { window.alert("Success! Check your email to confirm your subscription."); };
+});
